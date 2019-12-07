6c45525f74db2879eeef084886a0a3e5282f489f
noemiandor <noemi.andor@gmail.com>
2019-12-07 08:22:39 -0500

    Fixed performance of getSubProfiles -- single sql access

--- rpackage/R/getSubProfiles.R
+++ rpackage/R/getSubProfiles.R
@@ -3,34 +3,13 @@ getSubProfiles<-function (cloneID_or_sampleName, whichP = "TranscriptomePerspect
   persp = J("core.utils.Perspectives")$valueOf(whichP)
   whichP_=gsub("Exome","",gsub("Genome","",gsub("Transcriptome","",whichP)))
   
-  yml = read_yaml(paste0(system.file(package='cloneid'), '/config/config.yaml'))
-  mydb = dbConnect(MySQL(), user=yml$mysqlConnection$user, password=yml$mysqlConnection$password, dbname=yml$mysqlConnection$database,host=yml$mysqlConnection$host, port=as.integer(yml$mysqlConnection$port))
-  
-  ##@TODO: doesn't work as expected when using root ID as input --> try accessing parent instead
+  cloneID = cloneID_or_sampleName;
   if (class(cloneID_or_sampleName) == "character") {
     cloneID = getRootID(cloneID_or_sampleName, whichP)
-  }  else {
-    cloneID = cloneID_or_sampleName
   }
   
-  stmt = paste0("select cloneID from ",whichP_," where parent =",cloneID);
-  rs = dbSendQuery(mydb, stmt)
-  kids = fetch(rs, n=-1)[,"cloneID"]
-  
-  # kids= kids[!is.na(kids)]
-  # kids =strsplit(kids,",")[[1]]
-  if(includeRoot) {
-    kids = c(cloneID, kids)
-  }
-  
-  dat=c()
-  for(kid in  as.integer(kids)){
-    p <- .jcall("cloneid.Manager", returnSig = "Ljava/util/Map;", method = "profile", kid, persp)
-    dat = cbind(dat, .javamap2Rmatrix(p))
-  }
-  
-  dbClearResult(dbListResults(mydb)[[1]])
-  dbDisconnect(mydb)
+  p <- .jcall("cloneid.Manager", returnSig = "Ljava/util/Map;", method = "profiles", as.integer(cloneID), persp, as.logical(includeRoot))
+  dat = .javamap2Rmatrix(p);
   
   return(dat)
 }
