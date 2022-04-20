6f441b51f569beaffb43918d43819d65a39281a9
noemiandor <noemi.andor@gmail.com>
2022-04-20 14:10:44 -0400

    NCI-N87 at 20X

--- rpackage/R/InVitroUtils.R
+++ rpackage/R/InVitroUtils.R
@@ -550,13 +550,14 @@ plotLiquidNitrogenBox <- function(rack, row){
   runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 1.0,  \"backgroundRadiusMicrons\": 15.0,  \"medianRadiusMicrons\": 0.0,  \"sigmaMicrons\": 1.5,  \"minAreaMicrons\": 2.0,  \"maxAreaMicrons\": 1000.0,  \"threshold\": 0.1,  \"maxBackground\": 2.9,  \"watershedPostProcess\": false,  \"cellExpansionMicrons\": 2.5,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
   # # NCI-N87 pipeline:
   if(cellLine=="NCI-N87"){
-    runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 0.5,  \"backgroundRadiusMicrons\": 8.0,  \"medianRadiusMicrons\": 0.0,  \"sigmaMicrons\": 1.5,  \"minAreaMicrons\": 40.0,  \"maxAreaMicrons\": 400.0,  \"threshold\": 0.09,  \"maxBackground\": 3.0,  \"watershedPostProcess\": false,  \"cellExpansionMicrons\": 5.0,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
+    # runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 0.5,  \"backgroundRadiusMicrons\": 8.0,  \"medianRadiusMicrons\": 0.0,  \"sigmaMicrons\": 1.5,  \"minAreaMicrons\": 40.0,  \"maxAreaMicrons\": 400.0,  \"threshold\": 0.09,  \"maxBackground\": 3.0,  \"watershedPostProcess\": false,  \"cellExpansionMicrons\": 5.0,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
+    runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 0.461,  \"backgroundRadiusMicrons\": 12.0,  \"medianRadiusMicrons\": 0.0,  \"sigmaMicrons\": 1,  \"minAreaMicrons\": 5,  \"maxAreaMicrons\": 200.0,  \"threshold\": 0.2,  \"maxBackground\": 3.0,  \"watershedPostProcess\": true,  \"cellExpansionMicrons\": 1.0,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
   }else if(cellLine=="HGC-27"){
     runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 0.5,  \"backgroundRadiusMicrons\": 8.0,  \"medianRadiusMicrons\": 0.0,  \"sigmaMicrons\": 1.5,  \"minAreaMicrons\": 90.0,  \"maxAreaMicrons\": 1200.0,  \"threshold\": 0.1,  \"maxBackground\": 2.0,  \"watershedPostProcess\": false,  \"cellExpansionMicrons\": 5.0,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
   }else if (cellLine=="SNU-16"){
     runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 0.5,  \"backgroundRadiusMicrons\": 8.0,  \"medianRadiusMicrons\": 0.0,  \"sigmaMicrons\": 3.5,  \"minAreaMicrons\": 40.0,  \"maxAreaMicrons\": 800.0,  \"threshold\": 0.1,  \"maxBackground\": 2.0,  \"watershedPostProcess\": false,  \"cellExpansionMicrons\": 5.0,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
-  }else if (cellLine=="NUGC-4"){
-    runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 1.0,  \"backgroundRadiusMicrons\": 15.0,  \"medianRadiusMicrons\": 0.0,  \"sigmaMicrons\": 1.5,  \"minAreaMicrons\": 2.0,  \"maxAreaMicrons\": 1000.0,  \"threshold\": 0.1,  \"maxBackground\": 2.9,  \"watershedPostProcess\": false,  \"cellExpansionMicrons\": 2.5,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
+  }else  if(cellLine=="NUGC-4"){
+    runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 0.922,  \"backgroundRadiusMicrons\": 8.0,  \"medianRadiusMicrons\": 2.0,  \"sigmaMicrons\": 1.5,  \"minAreaMicrons\": 10.0,  \"maxAreaMicrons\": 200.0,  \"threshold\": 0.1,  \"maxBackground\": 2.9,  \"watershedPostProcess\": true,  \"cellExpansionMicrons\": 2.5,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
   }else if(cellLine=="KATOIII"){
     runPlugin = "runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{\"detectionImageBrightfield\": \"Hematoxylin OD\",  \"requestedPixelSizeMicrons\": 0.5,  \"backgroundRadiusMicrons\": 8.0,  \"medianRadiusMicrons\": 0.0,  \"sigmaMicrons\": 1.5,  \"minAreaMicrons\": 40.0,  \"maxAreaMicrons\": 1200.0,  \"threshold\": 0.09,  \"maxBackground\": 3.0,  \"watershedPostProcess\": true,  \"cellExpansionMicrons\": 5.0,  \"includeNuclei\": false,  \"smoothBoundaries\": true,  \"makeMeasurements\": true}');"
   }
@@ -597,14 +598,27 @@ plotLiquidNitrogenBox <- function(rack, row){
         ," double pixelWidth = cal.getPixelWidthMicrons();"
         ," double pixelHeight = cal.getPixelHeightMicrons();"
         ," "
-        ," print(pixelWidth);"
-        ," print(pixelHeight);"
         ," "
-        ," "
-        ," if(pixelWidth == Double.NaN){"
-        ,"     setPixelSizeMicrons(PixelWidth_new, PixelWidth_new);"
+        ," def filename = entry.getImageName() + '.csv'"
+        , "// @TODO: read this info directly from .tif metadata"
+        ,"if(pixelWidth == Double.NaN){"
+        ," if (filename.contains('_20x_')){"
+        ,"   print('20x data');"
+        ,"   PixelWidth_new = 200/433.77;"
+        ,"   print(PixelWidth_new);"
+        ,"   setPixelSizeMicrons(PixelWidth_new, PixelWidth_new);"
+        ," }else if (filename.contains('_10x_')){"
+        ,"   print('10x data');"
+        ,"   PixelWidth_new = 400/433.77;"
+        ,"   print(PixelWidth_new);"
+        ,"   setPixelSizeMicrons(PixelWidth_new, PixelWidth_new);"
+        ," }else if (filename.contains('_40x_')){"
+        ,"   print('40x data');"
+        ,"   PixelWidth_new = 100/433.77;"
+        ,"   print(PixelWidth_new);"
+        ,"   setPixelSizeMicrons(PixelWidth_new, PixelWidth_new);"
         ," }"
-        ," "
+        ,"}"
         ," "
         ," setImageType('BRIGHTFIELD_H_E');"
         ," setColorDeconvolutionStains('{\"Name\" : \"H&E default\", \"Stain 1\" : \"Hematoxylin\", \"Values 1\" : \"0.65111 0.70119 0.29049 \", \"Stain 2\" : \"Eosin\", \"Values 2\" : \"0.2159 0.8012 0.5581 \", \"Background\" : \" 255 255 255 \"}');"
@@ -617,7 +631,6 @@ plotLiquidNitrogenBox <- function(rack, row){
         ," selectAnnotations();"
         , runPlugin
         ," "
-        ," def filename = entry.getImageName() + '.csv'"
         ," selectDetections()"
         , paste0("def pathDetection = buildFilePath('",qpdir,"/pred');")
         , paste0("def pathAnnotation = buildFilePath('",qpdir,"/cellpose_count')")
---
+++ rpackage/inst/python/preprocessing.py
@@ -0,0 +1,82 @@
+
+import sys
+import os 
+import cv2 as cv
+import numpy as np
+from tqdm import tqdm
+import ntpath
+
+# To call this code do python3 preprocessing.py  <path2Image/Image.tif>  <cellLine>   <threshold>
+# threshold is not a required argument and the default value for threshold is 40 
+
+
+def equalize_img(img):
+    if (len(img.shape)< 3):
+        img_eq = cv.equalizeHist(img)
+    else:
+        R,G,B = cv.split(img)
+        R_eq = cv.equalizeHist(R)
+        G_eq = cv.equalizeHist(G)
+        B_eq = cv.equalizeHist(B)
+
+        img_eq = cv.merge((R_eq,G_eq,B_eq))
+        return img_eq
+
+def gammaCorrection(img_original):
+    gamma = 0.4
+    lookUpTable = np.empty((1,256), np.uint8)
+    for i in range(256):
+        lookUpTable[0,i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
+
+    res = cv.LUT(img_original, lookUpTable)
+    return res
+
+def ApplyGammaCorrection(path2Image,cellLine,th=40):
+    cellLine = cellLine.upper()
+    if not isinstance(th,int):
+        th=int(th)
+    img = cv.imread(os.path.join(path2Image))
+    if img is None:
+        print('image reading is not right')
+        sys.exit()
+    else:
+        imgName=ntpath.basename(path2Image) 
+        print(np.mean(img))
+        if np.mean(img) < th:
+            if cellLine.startswith('SNU') or cellLine.startswith('HGC'):
+                img = gammaCorrection(img)
+                img = equalize_img(img)   
+                img = cv.GaussianBlur(img,(5,5),0) 
+            elif cellLine.startswith('NCI'):
+                img = gammaCorrection(img)
+                img = np.invert(img)
+        # apply inverstion of KATOIII without the need for thresholding         
+        if cellLine.startswith('KATO'):
+            img = np.invert(img)
+        cv.imwrite(os.path.join(path2Image),img)
+
+def GetImageNames(pathToImages,th=40):
+    images = os.listdir(pathToImages)
+    for img in tqdm(images):
+        if img.startswith('.'):
+            continue
+        else:
+            ApplyGammaCorrection(pathToImages,img,th)
+
+def main():
+    args = len(sys.argv)
+    if args < 2:
+        print("Error: enter image path including its name and type of cellLine and threshold")
+        print('Example: python3 preprocessing.py  <path2Image/Image.tif>  <cellLine>   <threshold>')
+        print('Note: the default value for threshold is 40, and threshold is not a required argument')
+    elif args < 3:
+        print("Error: enter image path including its name and type of cellLine and threshold")
+        print('Example: python3 preprocessing.py  <path2Image/Image.tif>  <cellLine>   <threshold>')
+        print('Note: the default value for threshold is 40, and threshold is not a required argument')
+    elif args < 4:
+        ApplyGammaCorrection(sys.argv[1],sys.argv[2])
+    else:
+        ApplyGammaCorrection(sys.argv[1],sys.argv[2],sys.argv[3])
+
+if __name__ == "__main__":
+    main()
