7026c8d26a1f5a91b829b0abe93ed9a20e140485
Noemi <noemi.andor@gmail.com>
2022-09-26 21:53:28 -0400

    remove expands dependency

--- rpackage/R/Utils.R
+++ rpackage/R/Utils.R
@@ -105,4 +105,24 @@ dm=dm[,colnames(dm)!="segmentLength",drop=F];
 return(dm);
 }
 
+.addColumn<-function(M,newCol,initVal){
+  if (!any(colnames(M)==newCol)){
+    if(!is.null(dim(M))){
+      M=matrix(cbind(M,matrix(initVal,nrow(M),1)),nrow=nrow(M),ncol=ncol(M)+1,
+               dimnames = list(rownames(M), c(colnames(M),newCol)));
+    }else{
+      cols=names(M);
+      M=c(M,initVal);
+      names(M)=c(cols,newCol);
+    }
+  }
+  return(M);
+}
 
+                
+.notifyUser<-function(message,verbose=T){
+  if(verbose){
+    print(message)
+  }
+}
+                
