0f4e6e45c899c6acd9d834ac70f9f63facf3e667
noemiandor <noemi.andor@gmail.com>
2022-10-12 13:40:49 -0400

    correcting area calculations for CellPose

--- rpackage/inst/python/GetCount_cellPose.py
+++ rpackage/inst/python/GetCount_cellPose.py
@@ -1,7 +1,7 @@
 #post-processing prediction
 
 #to run this file do 
-#python GetCount_cellPose.py  <path to images>  <path to pretrained model> <path to save results>  <extention of the original images such as '.png'>
+#python GetCount_cellPose.py  <path to images>  <path to pretrained model> <path to save results>  <extention of the original images such as '.tif'>
 import argparse
 import os 
 import cv2
@@ -11,7 +11,36 @@ from tqdm import tqdm
 from cellpose.plot import mask_overlay
 import sys
 from subprocess import call 
+from PIL import Image
+from PIL.TiffTags import TAGS
 
+def get_metadata(path2Image):
+    img = Image.open(path2Image)
+    meta_dict = {TAGS[key] : img.tag[key] for key in img.tag_v2}    
+    #print(meta_dict)
+    try:
+        objective_lens = meta_dict['ImageDescription'][0].split(' ')[1]
+        objective_lens = objective_lens.split('"')[1]
+        #print('objectivelens from meta data {}'.format(objective_lens))
+    except:
+        if len(path2Image.split('/')) > 1:
+            imageName = path2Image.split('/')[-1]
+            if '10x' in imageName:
+                objective_lens = '10x'
+            elif '20x' in imageName:
+                objective_lens = '20x'
+            elif '40x' in imageName:
+                objective_lens = '40x'
+        else:
+            imageName = path2Image
+            if '10x' in imageName:
+                objective_lens = '10x'
+            elif '20x' in imageName:
+                objective_lens = '20x'
+            elif '40x' in imageName:
+                objective_lens = '40x'
+        #print('objectivelens from filename {}'.format(objective_lens))
+    return objective_lens
 
 def vis_overlay(path2Masks,path2Save,ext):
   #parentPath = os.path.dirname(path2Masks)
@@ -34,7 +63,7 @@ def vis_overlay(path2Masks,path2Save,ext):
       cv2.imwrite(os.path.join(path2Save,'vis',base_name+'_overlay.png'),overlay)
 
 
-def get_blob_prop(msk):
+def get_blob_prop(msk,pixel_size):
   contours,hierarchy = cv2.findContours(msk, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
   for cnt in contours:
     try:
@@ -48,25 +77,32 @@ def get_blob_prop(msk):
       perimeter = cv2.arcLength(cnt,True)
     except:
       continue  
-    return {'Centroid X µm':x, 'Centroid Y µm':y,'Area µm^2':area,'perimeter µm':perimeter,'ROI':ROI}
+    area = area * pixel_size * pixel_size
+    return {'Centroid X µm':x * pixel_size, 'Centroid Y µm':y * pixel_size,'Area µm^2':area,'perimeter µm':perimeter * pixel_size,'ROI':ROI}
 
-def get_ROI_cellCount(df,msk,name):
+def get_ROI_cellCount(df,msk,name,pixel_size):
   df_total_det = pd.DataFrame()
   total_detection = df.shape[0]
   image_name = name.split('_cp_masks')[0]+'.tif'
   [d1,d2] = msk.shape
   area = d1 * d2
-  perimeter= d1 + d1 + d2 + d2
+  
+  area = area * pixel_size * pixel_size 
+
+  perimeter = d1 + d1 + d2 + d2
+  perimeter = perimeter * pixel_size
   Centroid_X = d1/2
+  Centroid_X = Centroid_X * pixel_size
   Centroid_Y = d2/2
+  Centroid_Y = Centroid_Y * pixel_size
   ROI = 'rectangle'
   df_total_det['Image Name'] = [image_name]
   df_total_det['ROI'] = [ROI]
-  df_total_det['Centroid X µm'] = [Centroid_X]
+  df_total_det['Centroid X µm'] = [Centroid_X] 
   df_total_det['Centroid Y µm'] = [Centroid_Y]
   df_total_det['Num Detections'] = [total_detection]
-  df_total_det['Area µm^2'] = [area]
-  df_total_det['perimeter µm'] = [perimeter]
+  df_total_det['Area µm^2'] = [area] 
+  df_total_det['perimeter µm'] = [perimeter] 
   return df_total_det
 
 def getCount(path2Pred):
@@ -93,8 +129,17 @@ def get_count2csv(list_of_cells_props):
     df['perimeter µm'] = [i['perimeter µm'] for i in list_of_cells_props]
     return df
 
+def get_pixel_size(objectiveLens):
+    if objectiveLens == '10x':
+        pixel_size = 0.922 
+    elif objectiveLens == '20x':
+        pixel_size = 0.922 / float(2)
+    elif objectiveLens == '40x':
+        pixel_size = 0.922 / float(4) 
+    return pixel_size
 
-def iterate(path2Pred,path2Save):
+
+def iterate(path2Pred,path2Save,ext):
   #parentPath = os.path.dirname(path2Pred)
   if not os.path.exists(os.path.join(path2Save,'pred')):
     os.makedirs(os.path.join(path2Save,'pred'))
@@ -107,19 +152,25 @@ def iterate(path2Pred,path2Save):
       if not maskName.endswith('_cp_masks.png'):
         continue 
       else:
+          if not ext.startswith('.'):
+            ext = '.'+ext
           mask = cv2.imread(os.path.join(path2Pred,maskName),-1)
+          image_name = maskName.split('_cp_masks.png')[0]+ext
+          path2Image = os.path.join(path2Pred,image_name)
+          objective_len = get_metadata(path2Image)
+          pixel_size = get_pixel_size(objective_len)
           list_of_cells_props = []
           for i in range(mask.max()):
               msk =(mask == i+1)*255
               msk = msk[100:1200,100:1900]
               nzCount = cv2.countNonZero(msk)
               if(nzCount > 0):
-                  prop_dict = get_blob_prop(msk.astype(np.uint8))
+                  prop_dict = get_blob_prop(msk.astype(np.uint8),pixel_size)
                   if prop_dict:
                       list_of_cells_props.append(prop_dict)
 
           df = get_count2csv(list_of_cells_props)
-          df_total = get_ROI_cellCount(df,msk,maskName)
+          df_total = get_ROI_cellCount(df,msk,maskName,pixel_size)
           df.to_csv(os.path.join(path2Save,'pred',maskName.split('_cp_masks')[0]+'.csv'),index=False,sep='\t')
           df_total.to_csv(os.path.join(path2Save,'cellpose_count',maskName.split('_cp_masks')[0]+'.csv'),index=False,sep='\t')
           
@@ -128,6 +179,16 @@ def run_cellPose(path2Images,path2Pretrained):
 
 def run(path2Images,path2Pretrained,path2Save,ext):
   run_cellPose(path2Images,path2Pretrained)
-  iterate(path2Images,path2Save)
+  iterate(path2Images,path2Save,ext)
   vis_overlay(path2Images,path2Save,ext)
 
+'''
+if __name__ == "__main__":
+    # execute only if run as a script
+    args = len(sys.argv)
+    print(args)
+    if args == 5:
+      run(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+    else:
+      print('Error in number of arguments')
+'''
