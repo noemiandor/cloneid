c73ef40313ed219e7095533ed463a0a28620cbc4
Veith <tveith@usf.edu>
2024-08-09 10:39:15 -0400

    first commit on test_tommy branch

--- RUNNING.R
+++ RUNNING.R
@@ -7,23 +7,15 @@ options(java.parameters = "-Xmx9g")
 suppressWarnings(suppressMessages(library(cloneid)))
 library(tictoc)
 
-cat(paste0("\n", "\n"))
-
 sqlserver <- 'remote'
 
 sqlsetup <- switch(sqlserver,
-        docker = setupCLONEID(host='sql2', port='3306', user='xxxxx', password='xxxxx', database='CLONEID', schemaScript='CLONEID_schema.sql'),
-        remote = setupCLONEID(host='xxxxxxxxxxxxx.xxxxxxxxxxxx.us-east-1.rds.amazonaws.com', port='3306', user='xxxxx', password='xxxxx', database='CLONEID', schemaScript='CLONEID_schema.sql')
+                   docker = setupCLONEID(host='sql2', port='3306', user='thomasveith', password='clonesandsuch', database='CLONEID', schemaScript='CLONEID_schema.sql'),
+                   remote = setupCLONEID(host='cloneredesign.cswgogbb5ufg.us-east-1.rds.amazonaws.com', port='3306', user='thomasveith', password='clonesandsuch', database='CLONEID', schemaScript='CLONEID_schema.sql')
 )
-
-cat(paste0("### Sql Info : "))
-cat(paste0("\n", sqlserver, "\n", sqlsetup[1], "\n", sqlsetup[2], "\n"))
-cat(paste0("###", "\n", "\n"))
 
 # Specify the clone ID for which we want to find descendants.
 cl <- "SNU-668_A9_seed"
-whichP = "GenomePerspective"
-# whichP = "TranscriptomePerspective"
 
 # Find all descendands of the specified clone ID, excluding any recursive results.
 out <- suppressWarnings(suppressMessages(findAllDescendandsOf(ids = cl, recursive = FALSE)))
@@ -31,27 +23,26 @@ out <- suppressWarnings(suppressMessages(findAllDescendandsOf(ids = cl, recursiv
 ## GenomePerspective
 ## Do we have Genome sequencing data for any lineage from this cell line?
 
-stmt = paste0("select distinct origin from Perspective where whichPerspective='",whichP,"' and sampleSource = '",unique(out$cellLine),"'")
+stmt = paste0("select distinct origin from Perspective where whichPerspective='GenomePerspective' and sampleSource = '",unique(out$cellLine),"'")
 mydb = cloneid::connect2DB()
 rs <- suppressWarnings(dbSendQuery(mydb, stmt))
 origin=fetch(rs, n=-1)[,"origin"]
 
-
 ## Download genomic profile for one subpopulation:
 
-report <- paste0("### ",whichP," : ", origin)
+report <- paste0("### GenomePerspective : ", origin)
 cat(paste0("\n", report, "\n"))
 #timing
 tic(report)
 
 # Get the subclones from the origin
-sps <- getSubclones(cloneID_or_sampleName = origin, whichP = whichP)
+sps <- getSubclones(cloneID_or_sampleName = origin, whichP = "GenomePerspective")
 
 # Extract the genomic profiles for each subclone
-p <- sapply(names(sps), function(x) { y<-as.numeric(extractID(x)); cat(paste0("Processing SP", y, "\n")); cloneid::getSubProfiles(cloneID_or_sampleName = y, whichP = whichP) })
+p <- sapply(names(sps), function(x) { y<-as.numeric(extractID(x)); cat(paste0("Processing SP", y, "\n")); cloneid::getSubProfiles(cloneID_or_sampleName = y, whichP = "GenomePerspective") })
 
 #timing
-secs=toc()
+toc()
 cat(paste0("\n", "\n"))
 
 # Get the clone membership information
@@ -62,7 +53,6 @@ clonesizes <- sapply(p,ncol)
 
 # Combine the genomic profiles into a single data frame
 p <- do.call(cbind, p)
-print(paste(((secs$toc-secs$tic)/60)/ncol(p), "minutes per cell for",whichP) ) 
 
 # Print the dimensions of the combined data frame
 cat(paste('dimensions', dim(p)[1], dim(p)[2], "\n"))
@@ -74,4 +64,3 @@ print(head(p[,1:min(3,ncol(p))]))
 cat("\nPress [enter] to exit")
 invisible(readLines("stdin", n=1))
 quit()
-
