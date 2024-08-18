7e9e78b67651db94ba6831b9d5d038c47ad08823
noemiandor <noemi.andor@gmail.com>
2024-08-18 01:03:18 -0400

    fixing bug in buildCellPoseEnvironment

--- rpackage/R/buildCellPoseEnvironment.R
+++ rpackage/R/buildCellPoseEnvironment.R
@@ -1,6 +1,6 @@
 buildCellPoseEnvironment = function(){
+  confirmError = ""
   if('cellpose' %in% reticulate::conda_list()$name){
-    confirmError = ""
     while(!confirmError %in% c("yes", "no")){
       confirmError <- readline(prompt="A conda environment named 'cellpose' already exists. Are you sure you want to delete it and install a new one? This is typically only necessary when installing cloneid for the first time. Type 'yes' to continue, 'no' to abort: ")
     }
