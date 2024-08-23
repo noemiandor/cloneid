45953255c05625829d6b3a852e3137e2a20d8e46
noemiandor <noemi.andor@gmail.com>
2024-08-23 18:33:58 -0400

    fixed bug in buildCellPoseEnvironment.R

--- rpackage/R/buildCellPoseEnvironment.R
+++ rpackage/R/buildCellPoseEnvironment.R
@@ -4,6 +4,8 @@ buildCellPoseEnvironment = function(){
     while(!confirmError %in% c("yes", "no")){
       confirmError <- readline(prompt="A conda environment named 'cellpose' already exists. Are you sure you want to delete it and install a new one? This is typically only necessary when installing cloneid for the first time. Type 'yes' to continue, 'no' to abort: ")
     }
+  }else{
+    confirmError = "yes"
   }
   if(tolower(confirmError)=="yes"){
     system("conda env remove -n cellpose")
